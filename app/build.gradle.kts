9387a27d47a04a579b3c08bd4cb721f064d94c97
Victor Hidalgo <victor.hidalgo@proton.ch>
2025-09-17 14:12:15 +0200

    bump: version 1.2.0 → 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
 val sentryDSN: String? = System.getenv("SENTRY_DSN")
 
 val jobId: Int = System.getenv("CI_JOB_ID")?.take(3)?.toInt() ?: 0
-val appVersionName: String = "1.2.0"
+val appVersionName: String = "1.2.1"
 val appVersionCode: Int = versionCode(appVersionName)
 
 fun versionCode(versionName: String): Int {
